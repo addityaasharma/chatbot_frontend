f6c63133e3ede708475b782c0548f14a4c6e6599
addityaasharma <adityasharma96655@gmail.com>
2025-07-30 12:14:36 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
   },
+  base: './', // ✅ Important for Vercel or GitHub Pages
 })
